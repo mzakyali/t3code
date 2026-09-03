61a4da984367f48b4491375ab0bf22635a0c436f
Muhammad Zaky <mhmmdzakya30@gmail.com>
2026-09-03 14:34:17 +0700

    fix: resolve Devin rebase validation errors

--- apps/web/src/components/ChatView.tsx
+++ apps/web/src/components/ChatView.tsx
@@ -340,7 +340,6 @@ import {
   deriveLatestContextWindowSnapshot,
   formatContextWindowTokens,
 } from "../lib/contextWindow";
-import { ThreadSyncStatusPill } from "./chat/ThreadSyncStatusPill";
 import {
   DRAFT_HERO_TRANSITION_ANIMATION_ID,
   DRAFT_HERO_TRANSITION_DURATION_MS,
@@ -5537,7 +5536,6 @@ function ChatViewContent(props: ChatViewProps) {
       return [
         ...systemComposerBannerItems,
         ...backgroundLivenessItems,
-        ...calmSystemItems,
         ...reinitItems,
         ...resumeCompactionItems,
         ...wokeThreadItems,
@@ -5547,7 +5545,6 @@ function ChatViewContent(props: ChatViewProps) {
     return [
       ...systemComposerBannerItems,
       ...backgroundLivenessItems,
-      ...calmSystemItems,
       ...reinitItems,
       ...resumeCompactionItems,
       ...wokeThreadItems,
